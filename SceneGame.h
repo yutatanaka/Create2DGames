1d7cf36422e0a414fbff3ae0c41a87de1377b531
yutatanaka <koryotennis99@gmail.com>
2016-02-06 13:58:30 +0900

    SceneGameクラスを作成、メンバ関数、変数を追加

---
+++ SceneGame.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+class SceneGame
+{
+public:
+
+	// コンストラクタ
+	SceneGame();
+
+	// デストラクタ
+	~SceneGame();
+
+	// 初期化メソッド
+	void Initialize();
+
+	// 更新メソッド
+	void Update();
+
+	// 描画メソッド
+	void Draw();
+};
