6877fc4171870ca5c54f14db70c7ccb529796a7f
yutatanaka <koryotennis99@gmail.com>
2016-02-06 14:04:24 +0900

    SceneGameOverクラスを作成、メンバ関数、変数を追加

---
+++ SceneGameOver.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+class SceneGameOver
+{
+public:
+
+	// コンストラクタ
+	SceneGameOver();
+
+	// デストラクタ
+	~SceneGameOver();
+
+	// 初期化メソッド
+	void Initialize();
+
+	// 更新メソッド
+	void Update();
+
+	// 描画メソッド
+	void Draw();
+};
