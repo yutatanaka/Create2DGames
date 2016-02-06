1b0773067d1a4b1250eac64de00131861e0b49cc
yutatanaka <koryotennis99@gmail.com>
2016-02-06 14:01:13 +0900

    SceneClearクラスを作成、メンバ関数、変数を追加

---
+++ SceneClear.h
@@ -0,0 +1,21 @@
+
+#pragma once
+
+class SceneClear
+{
+public:
+	// コンストラクタ
+	SceneClear();
+
+	// デストラクタ
+	~SceneClear();
+
+	// 初期化メソッド
+	void Initialize();
+
+	// 更新メソッド
+	void Update();
+
+	// 描画メソッド
+	void Draw();
+};
