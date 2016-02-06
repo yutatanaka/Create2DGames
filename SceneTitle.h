f0959546a1278ea52046afe07410fd8e36a5156a
yutatanaka <koryotennis99@gmail.com>
2016-02-06 13:55:38 +0900

    SceneTitleクラスを作成、ﾒﾝﾊﾞ関数、変数を追加

---
+++ SceneTitle.h
@@ -0,0 +1,22 @@
+
+#pragma once 
+
+class SceneTitle
+{
+public:
+
+	// コンストラクタ
+	SceneTitle();
+
+	// デストラクタ
+	~SceneTitle();
+
+	// 初期化メソッド
+	void Initialize();
+
+	// 更新メソッド
+	void Update();
+
+	// 描画メソッド
+	void Draw();
+};
