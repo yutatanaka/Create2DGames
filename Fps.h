986d447c47ad224f3e45e0362088b5babbfa44ab
yutatanaka <koryotennis99@gmail.com>
2016-02-14 17:01:41 +0900

    FPSクラス内にメンバ関数、変数を追加

--- Fps.h
+++ Fps.h
@@ -7,5 +7,40 @@
 class Fps
 {
 
+public:
+
+	// コンストラクタ
+	Fps();
+
+	// デストラクタ
+	~Fps();
+
+	// 更新メソッド
+	bool Update();
+
+	// 描画メソッド
+	void Draw();
+
+	// かかった時間等を計測するメソッド
+	void Wait();
+
+public:
+
+	// 測定開始時刻
+	int startTime;
+
+	// カウンタ
+	int count;
+
+	// FPS
+	float fps;
+
+//////////////////////////////////////////
+
+	// 平均を取るサンプル数(定数)
+	static const int N = 60;
+
+	// 設定したFPS(定数)
+	static const int FPS = 60;
 };
 	
