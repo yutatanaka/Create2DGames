e20ab408b3b82a13fc71eb81a0f1d0f8215fcf28
yutatanaka <koryotennis99@gmail.com>
2016-02-14 16:28:05 +0900

    Soundクラスを追加、更にメンバ関数とそれぞれに使うメンバ変数を追加

---
+++ Sound.h
@@ -0,0 +1,40 @@
+
+#pragma once
+
+class Sound
+{
+private:
+
+	// コンストラクタ
+	Sound();
+
+	// デストラクタ
+	~Sound();
+
+public:
+
+	// 初期化処理メソッド
+	void Initialize();
+
+public:
+
+	// 決定ボタンなどを押したときのSE
+	int dicitionSoundHandle;
+
+	// ゲーム画面で流れるBGM
+	int gameMusicHandle;
+
+	// ゲームオーバーで流れるBGM
+	int gameOverMusicHandle;
+
+	// クリア画面で流れるBGM
+	int clearMusicHandle;
+
+	// プレイヤーが待機モーションの時のSE
+	int dogSoundHandle;
+
+	// プレイヤーがジャンプモーションの時のSE
+	int jumpSoundHandle;
+};
+
+extern Sound sound;
